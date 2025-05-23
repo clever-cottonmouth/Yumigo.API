ca9531ccb98517556c54b65e0bb92f4423500a8d
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-23 10:11:07 +0530

    Add project files.

---
+++ Yumigo.API/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yumigo.API.Models;
+
+namespace Yumigo.API.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+
+
+    }
+}
