032fdcd38341a585cb3786ee00b8de0543dd2292
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-24 11:10:07 +0530

    feat: static files added

---
+++ Yumigo.API/Models/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yumigo.API.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }= string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string SpecialTag {  get; set; } = string.Empty;
+        [Range(1,1000)]
+        public double Price { get; set; }
+        [Required]
+        public string Image {  get; set; } = string.Empty;
+    }
+}
