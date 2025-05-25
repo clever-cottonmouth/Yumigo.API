049c09fe5dd20e764eec79b70ceb1f4dab8d41b0
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-25 13:50:28 +0530

    feat: api setup

---
+++ Yumigo.API/Controllers/MenuItemController.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using Yumigo.API.DbContext;
+using Yumigo.API.Models;
+using Yumigo.API.Models.DTO;
+
+namespace Yumigo.API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ApiResponse _response;
+        private readonly IWebHostEnvironment _env;
+        public MenuItemController(ApplicationDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _response = new ApiResponse();
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuItems()
+        {
+            _response.Result = _context.menuItems.ToList();
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}", Name="GetMenuItem")]
+        public IActionResult GetMenuItem(int id)
+        {
+            if(id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            else
+            {
+                MenuItem? menuItem = _context.menuItems.FirstOrDefault(_ => _.Id == id);
+                _response.Result = menuItem;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (menuItemCreateDTO.File == null || menuItemCreateDTO.File.Length == 0)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.ErrorMessages = ["File is Required"];
+                        return BadRequest(_response);
+                    }
+                    var imagesPath = Path.Combine(_env.WebRootPath, "images");
+                    if (!Directory.Exists(imagesPath)) 
+                    {
+                        Directory.CreateDirectory(imagesPath);
+                    }
+                    var filePath = Path.Combine(imagesPath, menuItemCreateDTO.File.FileName);
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+                    //Uploading the image
+                    using(var stream= new FileStream(filePath, FileMode.Create))
+                    {
+                        await menuItemCreateDTO.File.CopyToAsync(stream);
+                    }
+
+                    MenuItem menuItem = new()
+                    {
+                        Name = menuItemCreateDTO.Name,
+                        Description = menuItemCreateDTO.Description,
+                        Price = menuItemCreateDTO.Price,
+                        Category = menuItemCreateDTO.Category,
+                        SpecialTag = menuItemCreateDTO.SpecialTag,
+                        Image = "images/" + menuItemCreateDTO.File.FileName,
+                    };
+                    _context.menuItems.Add(menuItem);
+                    await _context.SaveChangesAsync();
+
+                    _response.Result = menuItemCreateDTO;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem", new {id= menuItem.Id}, _response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(ex);
+            }
+            return BadRequest(_response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ApiResponse>> UpdateMenuItem(int id,[FromForm] MenuItemUpdateDTO menuItemUpdateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (menuItemUpdateDTO.File == null || menuItemUpdateDTO.Id!=id)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return BadRequest(_response);
+                    }
+
+                    MenuItem menuItemFromDb = await _context.menuItems.FirstOrDefaultAsync(_ => _.Id == id);
+
+                    if (menuItemFromDb == null)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return BadRequest(_response);
+                    }
+
+
+                    menuItemFromDb.Name = menuItemFromDb.Name;
+                    menuItemFromDb.Description = menuItemFromDb.Description;
+                    menuItemFromDb.Price=menuItemFromDb.Price;
+                    menuItemFromDb.Category = menuItemFromDb.Category;
+                    menuItemFromDb.SpecialTag = menuItemFromDb.SpecialTag;
+
+                    if (menuItemUpdateDTO.File != null && menuItemUpdateDTO.File.Length >0) 
+                    {
+                        var imagesPath = Path.Combine(_env.WebRootPath, "images");
+                        if (!Directory.Exists(imagesPath))
+                        {
+                            Directory.CreateDirectory(imagesPath);
+                        }
+                        var filePath = Path.Combine(imagesPath, menuItemUpdateDTO.File.FileName);
+                        if (System.IO.File.Exists(filePath))
+                        {
+                            System.IO.File.Delete(filePath);
+                        }
+
+                        var filePath_OldFile = Path.Combine(_env.WebRootPath, menuItemFromDb.Image);
+                        if (System.IO.File.Exists(filePath_OldFile))
+                        {
+                            System.IO.File.Delete(filePath_OldFile);
+                        }
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await menuItemUpdateDTO.File.CopyToAsync(stream);
+                        }
+
+                        menuItemFromDb.Image = "images/" + menuItemUpdateDTO.File.FileName;
+
+                        
+                    }
+
+                    _context.menuItems.Update(menuItemFromDb);
+                    await _context.SaveChangesAsync();
+
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(ex);
+            }
+            return BadRequest(_response);
+        }
+
+
+
+        [HttpDelete]
+        public async Task<ActionResult<ApiResponse>> DeleteMenuItem(int id)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (id==0)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return BadRequest(_response);
+                    }
+
+                    MenuItem? menuItemFromDb = await _context.menuItems.FirstOrDefaultAsync(_ => _.Id == id);
+
+                    if (menuItemFromDb == null)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return BadRequest(_response);
+                    }
+
+                    var filePath_OldFile = Path.Combine(_env.WebRootPath, menuItemFromDb.Image);
+                    if (System.IO.File.Exists(filePath_OldFile))
+                    {
+                        System.IO.File.Delete(filePath_OldFile);
+                    }
+
+                    _context.menuItems.Remove(menuItemFromDb);
+                    await _context.SaveChangesAsync();
+
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(ex);
+            }
+            return BadRequest(_response);
+        }
+    }
+}
--- Yumigo.API/Models/DTO/MenuItemUpdateDTO.cs
+++ Yumigo.API/Models/DTO/MenuItemUpdateDTO.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Yumigo.API.Models
+namespace Yumigo.API.Models.DTO
 {
-    public class MenuItem
+    public class MenuItemUpdateDTO
     {
         [Key]
         public int Id { get; set; }
@@ -15,10 +11,9 @@ namespace Yumigo.API.Models
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string Category { get; set; } = string.Empty;
-        public string? SpecialTag {  get; set; } 
-        [Range(1,1000)]
+        public string? SpecialTag { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
-        [Required]
-        public string Image {  get; set; } = string.Empty;
+        public IFormFile? File { get; set; }
     }
 }
