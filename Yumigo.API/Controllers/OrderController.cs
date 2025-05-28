29412d71e0eb4b391616922a8203946d1d4edb89
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-28 21:55:02 +0530

    feat: update order

--- Yumigo.API/Controllers/OrderController.cs
+++ Yumigo.API/Controllers/OrderController.cs
@@ -12,12 +12,12 @@ namespace Yumigo.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class OrderController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
         private readonly ApiResponse _response;
 
-        public ValuesController(ApplicationDbContext db, ApiResponse response)
+        public OrderController(ApplicationDbContext db, ApiResponse response)
         {
             _db = db;
             _response = response;
@@ -117,6 +117,93 @@ namespace Yumigo.API.Controllers
                 }
             }
             catch (Exception ex) 
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+        }
+
+
+
+        [HttpPut("{orderId:int}")]
+        public ActionResult<ApiResponse> UpdateOrder(int orderId,[FromBody] OrderHeaderUpdateDTO orderHeaderUpdateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (orderId!= orderHeaderUpdateDTO.OrderHeaderId)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.ErrorMessages.Add("Invalid Id");
+                        return BadRequest(_response);
+                    }
+
+
+
+                    OrderHeader? orderHeaderFromDb = _db.OrderHeaders
+                        .FirstOrDefault(_=>_.OrderHeaderId == orderId);
+
+                    if (orderHeaderFromDb == null)
+                    {
+                        _response.IsSuccess=!false;
+                        _response.StatusCode = HttpStatusCode.NotFound;
+                        _response.ErrorMessages.Add("Order not found");
+                        return NotFound(_response);
+                    }
+
+                    if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickUpName)) 
+                    {
+                        orderHeaderFromDb.PickUpName = orderHeaderUpdateDTO.PickUpName;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickUpPhoneNumber))
+                    {
+                        orderHeaderFromDb.PickUpPhoneNumber = orderHeaderUpdateDTO.PickUpPhoneNumber;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickUpEmail)) 
+                    {
+                        orderHeaderFromDb.PickUpEmail = orderHeaderUpdateDTO.PickUpEmail;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.Status))
+                    {
+                        if (orderHeaderFromDb.Status.Equals(SD.status_confirmed, StringComparison.InvariantCultureIgnoreCase)
+                            && orderHeaderUpdateDTO.Status.Equals(SD.status_readyForPickup, StringComparison.InvariantCultureIgnoreCase)
+                            )
+                        {
+                            orderHeaderFromDb.Status = SD.status_readyForPickup;
+                        }
+
+                        if (orderHeaderFromDb.Status.Equals(SD.status_confirmed, StringComparison.InvariantCultureIgnoreCase)
+                            && orderHeaderUpdateDTO.Status.Equals(SD.status_Completed, StringComparison.InvariantCultureIgnoreCase)
+                            )
+                        {
+                            orderHeaderFromDb.Status = SD.status_Completed;
+                        }
+
+                        if (orderHeaderFromDb.Status.Equals(SD.status_Cancelled, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            orderHeaderFromDb.Status = SD.status_Cancelled;
+                        }
+                    }
+
+                    _db.SaveChanges();
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+                   
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ModelState.Values.SelectMany(_ => _.Errors).
+                        Select(_ => _.ErrorMessage).ToList();
+                    return BadRequest(_response);
+
+                }
+            }
+            catch (Exception ex)
             {
                 _response.IsSuccess = false;
                 _response.StatusCode = HttpStatusCode.BadRequest;
--- Yumigo.API/Models/DTO/OrderHeaderUpdateDTO.cs
+++ Yumigo.API/Models/DTO/OrderHeaderUpdateDTO.cs
@@ -12,11 +12,8 @@ namespace Yumigo.API.Models.DTO
     {
         [Required]
         public int OrderHeaderId { get; set; }
-        [Required]
         public string PickUpName { get; set; } = string.Empty;
-        [Required]
         public string PickUpPhoneNumber { get; set; } = string.Empty;
-        [Required]
         public string PickUpEmail { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
     }
